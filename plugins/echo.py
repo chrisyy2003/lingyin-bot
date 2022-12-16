b60c13554d4b5f7f1e67c3d3fad40e868b0d73ef
chrisyy <1017975501@qq.com>
2022-12-16 22:25:16 +0800

    echo 增加回复事例

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,10 +1,12 @@
 from nonebot.rule import to_me
-from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import Message, MessageEvent, GroupMessageEvent, MessageSegment
+
 from nonebot.params import CommandArg
 from nonebot.plugin import on_command
 
 echo = on_command("echo", to_me())
 
 @echo.handle()
-async def handle_echo(message: Message = CommandArg()):
-    await echo.send(message=message)
+async def handle_echo(event: MessageEvent, message: Message = CommandArg()):
+    print(event.message_id)
+    await echo.send(MessageSegment.reply(event.message_id) + "good")
