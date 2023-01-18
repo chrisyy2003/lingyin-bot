efcc5a831cf309a6e40f41a5f3b0127592afc563
chrisyy <1017975501@qq.com>
2023-01-18 16:34:13 +0800

    update

--- plugins/echo.py
+++ plugins/echo.py
@@ -3,10 +3,10 @@ from nonebot.rule import to_me
 from nonebot.matcher import Matcher
 from nonebot.adapters import Message
 from nonebot.params import Arg, CommandArg, ArgPlainText
+from nonebot.adapters.onebot.v11 import Message, MessageEvent, PrivateMessageEvent, MessageSegment
 
-weather = on_command("1", aliases={"天气", "天气预报"}, priority=5)
+
+weather = on_command('test', priority=5)
 @weather.handle()
-async def handle_first_receive(matcher: Matcher, args: Message = CommandArg()):
-    plain_text = args.extract_plain_text()  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
-    if plain_text:
-        matcher.set_arg("city", args)  # 如果用户发送了参数则直接赋值
+async def handle_first_receive(matcher: Matcher, event: MessageEvent, args: Message = CommandArg()):
+    await matcher.finish(event.get_session_id())
